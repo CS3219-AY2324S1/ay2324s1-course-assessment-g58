f520736d2a19f36309bf0137c15c64f767671866
bokuanT <e0701815@u.nus.edu>
2023-09-11 16:17:30 +0800

    Move components into src and modify App.js and css to original application

--- frontend/peerprep/src/App.js
+++ frontend/peerprep/src/App.js
@@ -1,23 +1,19 @@
-import './App.css';
+import React from 'react';
+import Header from './components/LandingPage/Header';
+import QuestionForm from './components/LandingPage/QuestionForm';
+import QuestionTable from './components/LandingPage/QuestionTable';
+import Modal from './components/LandingPage/DescriptionModal';
+import './App.css';  
 
 function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+    return (
+        <main>
+            <Header />
+            <QuestionForm />
+            <QuestionTable />
+            <Modal />
+        </main>
+    );
 }
 
 export default App;
