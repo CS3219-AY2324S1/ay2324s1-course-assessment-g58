7eccda7fa2873fcab87ea7ef1272d188e1a72023
bokuanT <e0701815@u.nus.edu>
2023-09-16 23:47:54 +0800

    Convert loginbox and signupbox to tailwind

--- frontend/components/LandingPage/SignupBox.tsx
+++ frontend/components/LandingPage/SignupBox.tsx
@@ -1,71 +1,5 @@
-import React, { useState, CSSProperties } from 'react';
+import React, { useState } from 'react';
 import { useRouter } from 'next/router';
-import { useAuth } from '../../contexts/AuthContext';
-
-const styles: Record<string, CSSProperties> = {
-    main: {
-        height: '90vh',
-        width: '100vw',
-        position: 'relative',
-        margin: '0 auto',
-        fontFamily: "'Poppins', sans-serif",
-        backgroundColor: '#f0f2f5',
-        color: '#1c1e21'
-    },
-    row: {
-        display: 'flex',
-        justifyContent: 'center',
-        alignItems: 'center',
-        width: '100%',
-        maxWidth: '1000px',
-        position: 'absolute',
-        left: '50%',
-        top: '50%',
-        transform: 'translate(-50%, -50%)'
-    },
-    colmForm: {
-        flex: '0 0 40%',
-        textAlign: 'center'
-    },
-    formContainer: {
-        backgroundColor: '#ffffff',
-        border: 'none',
-        borderRadius: '10px',
-        boxShadow: '0 2px 4px rgba(0, 0, 0, 0.1), 0 8px 16px rgba(0, 0, 0, 0.1)',
-        padding: '20px',
-        maxWidth: '400px'
-    },
-    inputAndButton: {
-        width: '100%',
-        margin: '5px 0',
-        height: '45px',
-        verticalAlign: 'middle',
-        fontSize: '16px'
-    },
-    input: {
-        border: '1px solid #dddfe2',
-        color: '#1d2129',
-        padding: '0 8px',
-        outline: 'none',
-    },
-    btnLogin: {
-        backgroundColor: '#1877f2',
-        border: 'none',
-        borderRadius: '6px',
-        fontSize: '20px',
-        padding: '0 16px',
-        color: '#ffffff',
-        fontWeight: '700'
-    },
-    anchor: {
-        display: 'block',
-        color: '#1877f2',
-        fontSize: '14px',
-        textDecoration: 'none',
-        padding: '10px 0 20px',
-        borderBottom: '1px solid #dadde1',
-    }
-};
 
 const SignupBox: React.FC = () => {
     const router = useRouter();
@@ -75,39 +9,45 @@ const SignupBox: React.FC = () => {
     const [password, setPassword] = useState('');
 
     const handleSignupClick = async () => {
+        // Your signup logic here
     };
 
     return (
-        <div style={styles.main}>
-            <div style={styles.row}>
-                <div style={styles.colmForm}>
-                    <div style={styles.formContainer}>
-                        <h1>Signup</h1>
+        <div className="h-[90vh] w-screen relative mx-auto font-poppins bg-f0f2f5 text-1c1e21">
+            <div className="flex justify-center items-center w-full max-w-1000px absolute left-1/2 top-1/2 transform -translate-x-1/2 -translate-y-1/2">
+                <div className="flex-none text-center w-2/5">
+                    <div className="bg-white border-none rounded-xl shadow-sm p-5 max-w-md">
+                        <h1 className="text-2xl mb-5">Signup</h1>
                         <input
                             type="text"
                             placeholder="Full Name"
-                            style={{ ...styles.inputAndButton, ...styles.input}}
+                            className="w-full my-1 h-11 border border-dddfe2 text-1d2129 px-2 outline-none"
                             value={fullName}
                             onChange={e => setFullName(e.target.value)}
                         />
                         <input
                             type="text"
                             placeholder="Email address"
-                            style={{ ...styles.inputAndButton, ...styles.input}}
+                            className="w-full my-1 h-11 border border-dddfe2 text-1d2129 px-2 outline-none"
                             value={email}
                             onChange={e => setEmail(e.target.value)}
                         />
                         <input
                             type="password"
                             placeholder="Password"
-                            style={{ ...styles.inputAndButton, ...styles.input}}
+                            className="w-full my-1 h-11 border border-dddfe2 text-1d2129 px-2 outline-none"
                             value={password}
                             onChange={e => setPassword(e.target.value)}
                         />
-                        <button style={styles.btnLogin} onClick={handleSignupClick}>Sign Up</button>
+                        <button 
+                            className="w-full mt-1 h-11 bg-blue-600 rounded-md text-white text-xl font-bold"
+                            onClick={handleSignupClick}
+                        >
+                            Sign Up
+                        </button>
                         <a 
                             href="#"
-                            style={{ ...styles.anchor}}
+                            className="block mt-2.5 mb-5 text-1877f2 text-sm underline pb-2 border-b border-dadde1"
                             onClick={() => router.push('/login?mode=login')}
                         >
                             Already have an account? Log In
