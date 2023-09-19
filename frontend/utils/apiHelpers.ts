a4ef0624f9b95a6f2e34b2a3a428ce573478b592
ryanchua00 <ryanchuahj@gmail.com>
2023-09-19 21:08:36 +0800

    Use mintlify to generate docs

--- frontend/utils/apiHelpers.ts
+++ frontend/utils/apiHelpers.ts
@@ -6,6 +6,17 @@ export type ResponseData = {
     data?: any;
 }
 
+/**
+ * The below code provides helper functions for making HTTP requests using the fetch API in TypeScript.
+ * @param {string} url - The `url` parameter is a string that represents the URL of the API endpoint
+ * you want to make a request to. It should include the protocol (e.g., "http://" or "https://") and
+ * the domain name or IP address.
+ * @param {any} data - The `data` parameter is an object that you want to send to the above url for
+ * additional arguments.
+ * @returns The fetchGet, fetchPost, fetchPut, and fetchDelete functions all return a Promise that
+ * resolves to the parsed JSON response from the server.
+ */
+
 export async function fetchGet(url: string) {
     const res = await fetch(url);
     return res.json(); 
@@ -45,4 +56,22 @@ export async function fetchPut(url: string, data: any) {
         body: JSON.stringify(data), // body data type must match "Content-Type" header
     });
     return response.json(); // parses JSON response into native JavaScript objects
+}
+
+export async function fetchDelete(url: string, data: any) {
+        // Default options are marked with *
+        const response = await fetch(url, {
+            method: "DELETE", // *GET, POST, PUT, DELETE, etc.
+            mode: "cors", // no-cors, *cors, same-origin
+            cache: "no-cache", // *default, no-cache, reload, force-cache, only-if-cached
+            credentials: "same-origin", // include, *same-origin, omit
+            headers: {
+                "Content-Type": "application/json",
+                // 'Content-Type': 'application/x-www-form-urlencoded',
+            },
+            redirect: "follow", // manual, *follow, error
+            referrerPolicy: "no-referrer", // no-referrer, *no-referrer-when-downgrade, origin, origin-when-cross-origin, same-origin, strict-origin, strict-origin-when-cross-origin, unsafe-url
+            body: JSON.stringify(data), // body data type must match "Content-Type" header
+        });
+        return response.json(); // parses JSON response into native JavaScript objects
 }
