778984ae0d9c8f5557e9654a9f3432b7a70191d6
Varstak <e0725939@u.nus.edu>
2023-09-22 00:47:41 +0800

    Added MongoDB backend structure

---
+++ mongodb-database/src/index.ts
@@ -0,0 +1,34 @@
+import config from 'config';
+import express, { Request, Response } from 'express';
+import mongoose from 'mongoose';
+import { json } from 'body-parser';
+import { QuestionRouter } from './routes/question.routes';
+import dotenv from 'dotenv'
+dotenv.config()
+
+import { connectToDb } from './utils/connectToDb.utils';
+
+const port = config.get('port') as number; 
+const app = express();
+
+mongoose.connect("mongodb+srv://Alexander:ED9uP9nIh39CGxCW@cluster0.iza9wik.mongodb.net/questionBank?retryWrites=true&w=majority")
+
+const db = mongoose.connection
+
+db.on('error', console.error.bind(console, 'connection error:'))
+db.once('open', async () => {
+    console.log('Connected to DB ')
+})
+
+app.use(json());
+app.listen(port, async () => {
+    try {
+      console.log(`App is running at http://localhost:${port}`);
+      // rdiConnection();
+      // vqttConnection();
+      const dbConn = await connectToDb('DB_CONN_STRING');    
+    } catch (err: any) {
+      console.error("Error loading the app due to: ", err.message);
+    }
+  });
+app.use(QuestionRouter);
---
+++ mongodb-database/src/routes/question.routes.ts
@@ -0,0 +1,19 @@
+import express from 'express';
+import { json } from 'body-parser';
+import { createQuestion } from '../services/question.service';
+
+const router = express.Router();
+router.use(json());
+
+router.post('/api/new-question', async (req, res) => {
+    try {
+        const questionData = req.body;
+        const newQuestion = await createQuestion(questionData);
+        res.status(201).json(newQuestion);
+    } catch (err: any) {
+        res.status(500).json({ error: err.message });
+    }
+});
+
+export { router as QuestionRouter };
+
