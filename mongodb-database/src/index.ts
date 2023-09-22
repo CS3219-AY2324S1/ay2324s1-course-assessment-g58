872743cdae81c8b27043c9d1d08745f3b94982f4
bokuanT <e0701815@u.nus.edu>
2023-09-22 21:05:33 +0800

    Check that new-question route works with proper json body with Postman

--- mongodb-database/src/index.ts
+++ mongodb-database/src/index.ts
@@ -1,27 +1,3 @@
-// import config from 'config';
-// import express, { Request, Response } from 'express';
-// import mongoose from 'mongoose';
-// import { json } from 'body-parser';
-// import { QuestionRouter } from './routes/question.routes';
-// import dotenv from 'dotenv'
-// dotenv.config()
-
-// import { connectToDb } from './utils/connectToDb.utils';
-
-// const app = express();
-
-// mongoose.connect(process.env.DB_CONN_STRING as string);
-
-// const db = mongoose.connection
-
-// db.on('error', console.error.bind(console, 'connection error:'))
-// db.once('open', async () => {
-//     console.log('Connected to DB ')
-// })
-
-// app.use(json());
-// app.use(QuestionRouter);
-// External libraries and modules
 import express, { Application, Request, Response, NextFunction } from 'express';
 import mongoose from 'mongoose';
 import { json } from 'body-parser';
