53ed9461cb48ef11e897cd021cb881dd0ac50634
bokuanT <e0701815@u.nus.edu>
2023-09-25 01:48:05 +0800

    Use tailwind for question table

--- frontend/components/QuestionsPage/QuestionTable.tsx
+++ frontend/components/QuestionsPage/QuestionTable.tsx
@@ -5,58 +5,55 @@ interface QuestionTableProps {
     questions: Question[];
 }
 
-import { CSSProperties } from 'react';
-
-const styles: Record<string, CSSProperties> = {
-    table: {
-        borderCollapse: 'collapse',
-        width: '100%',
-        border: '3px solid black',
-        marginTop: '20px',
-        marginBottom: '20px'
-    },
-    th: {
-        backgroundColor: '#f2f2f2',
-        textAlign: 'left',
-        padding: '12px',
-        border: '1px solid black'
-    },
-    td: {
-        textAlign: 'left',
-        padding: '12px',
-        border: '1px solid black'
-    },
-    truncate: {
-        whiteSpace: 'nowrap',
-        overflow: 'hidden',
-        textOverflow: 'ellipsis',
-        maxWidth: '150px'
-    }
-};
-
 function QuestionTable({ questions }: QuestionTableProps) {
     console.log("refreshing question table", questions);
     return (
-        <table id="questionTable" style={styles.table}>
+        <table id="questionTable" 
+            className='w-full border-collapse border-4 border-black my-5'
+        >
             <thead>
                 <tr>
-                    <th style={styles.th}>Question Title</th>
-                    <th style={styles.th}>Question Complexity</th>
-                    <th style={styles.th}>Question Category</th>
-                    <th style={styles.th}>Question Description</th>
-                    <th style={styles.th}>Details</th>
-                    <th style={styles.th}>Delete</th>
+                    <th 
+                        className='bg-gray-200 text-left p-3 border border-black'
+                    >Question Title</th>
+                    <th
+                        className='bg-gray-200 text-left p-3 border border-black'
+                    >Question Complexity</th>
+                    <th
+                        className='bg-gray-200 text-left p-3 border border-black'                    
+                    >Question Category</th>
+                    <th
+                        className='bg-gray-200 text-left p-3 border border-black'                    
+                    >Question Description</th>
+                    <th
+                        className='bg-gray-200 text-left p-3 border border-black'                    
+                    >Details</th>
+                    <th
+                        className='bg-gray-200 text-left p-3 border border-black'                    
+                    >Delete</th>
                 </tr>
             </thead>
             <tbody id="questionTableBody">
                 {Array.isArray(questions) && questions.map(question => (
                     <tr key={question.title}>
-                        <td>{question.title}</td>
-                        <td>{question.difficulty}</td>
-                        <td>{question.category}</td>
-                        <td>{question.description}</td>
-                        <td>Details Button</td> {/* You can replace this with an actual button or link */}
-                        <td>Delete Button</td> {/* You can replace this with an actual button or link */}
+                        <td
+                            className='text-left p-3 border border-black truncate w-36'
+                        >{question.title}</td>
+                        <td
+                            className='text-left p-3 border border-black truncate w-36'
+                        >{question.difficulty}</td>
+                        <td
+                            className='text-left p-3 border border-black truncate w-36'
+                        >{question.category}</td>
+                        <td
+                            className='text-left p-3 border border-black truncate w-36'
+                        >{question.description}</td>
+                        <td
+                            className='text-left p-3 border border-black truncate w-36'
+                        >Details Button</td> {/* You can replace this with an actual button or link */}
+                        <td
+                            className='text-left p-3 border border-black truncate w-36'
+                        >Delete Button</td> {/* You can replace this with an actual button or link */}
                     </tr>
                 ))}
             </tbody>
