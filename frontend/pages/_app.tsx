948bbe23078c7da3a7a1db3006ece160b46bfdb3
bokuanT <e0701815@u.nus.edu>
2023-09-25 12:41:28 +0800

    Add back Ryan's changes

--- frontend/pages/_app.tsx
+++ frontend/pages/_app.tsx
@@ -6,9 +6,10 @@ import NavigationBar from '@/components/NavigationBar/NavigationBar';
 function MyApp({ Component, pageProps }: AppProps) {
   return (
     <MainContext>
-      <div className="app-container p-4">
-        <NavigationBar />
-        <main>
+      <div className="app-container h-[100vh]">
+        <NavigationBar /> 
+        {/* NavBar 6vh */}
+        <main className='p-4 h-[calc(100vh-50px)]'>
           <Component {...pageProps} />
         </main>
       </div>
