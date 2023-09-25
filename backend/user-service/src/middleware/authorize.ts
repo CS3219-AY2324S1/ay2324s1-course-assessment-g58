46fcb5d385e9ec905987830211ac3cb667847056
Teo Hao Yu <thstoneyes@gmail.com>
2023-09-25 15:02:38 +0800

    Update comments of code in authorize.ts

--- backend/user-service/src/middleware/authorize.ts
+++ backend/user-service/src/middleware/authorize.ts
@@ -4,19 +4,21 @@ import jwt, { JsonWebTokenError, TokenExpiredError } from "jsonwebtoken";
 type JwtVerifyError = JsonWebTokenError | TokenExpiredError;
 
 /**
- * The `authenticate` function is used to verify the authentication token in a request and attach the
- * user information to the request body.
+ * The `authenticate` function is used to verify the authentication token provided in the request and
+ * attach the user information to the request body if the token is valid.
  * @param {Request} req - The `req` parameter represents the HTTP request object, which contains
  * information about the incoming request such as headers, query parameters, and request body.
  * @param {Response} res - The `res` parameter is the response object that is used to send the HTTP
- * response back to the client. It contains methods and properties for manipulating the response, such
- * as setting the status code, sending JSON data, or redirecting the client to another URL.
+ * response back to the client. It contains methods and properties that allow you to set the response
+ * status, headers, and body. In this code, it is used to send JSON responses with appropriate status
+ * codes and error messages.
  * @param {NextFunction} next - The `next` parameter is a function that is used to pass control to the
  * next middleware function in the request-response cycle. It is typically called at the end of the
  * `authenticate` function to indicate that the authentication process is complete and the next
  * middleware function should be executed.
- * @returns In the code snippet, there are multiple return statements. Here is a breakdown of what is
- * being returned in each case:
+ * @returns In the code snippet provided, the function is returning a response object with a status
+ * code and a JSON object containing a message. The specific response being returned depends on the
+ * condition being evaluated.
  */
 export function authenticate(req: Request, res: Response, next: NextFunction) {
     try {
@@ -49,18 +51,20 @@ export function authenticate(req: Request, res: Response, next: NextFunction) {
 }
 
 /**
- * The `authorize` function is used to check if a user is authenticated and authorized to access a
- * certain route.
+ * The `authorize` function checks if a user is authenticated and authorized to access a route, based
+ * on a token provided in the request cookies.
  * @param {Request} req - The `req` parameter represents the HTTP request object, which contains
  * information about the incoming request such as headers, query parameters, and request body.
  * @param {Response} res - The `res` parameter is the response object that is used to send the HTTP
- * response back to the client. It contains methods and properties for manipulating the response, such
- * as setting the status code, sending JSON data, or redirecting the client to another URL.
+ * response back to the client. It contains methods and properties that allow you to set the response
+ * status, headers, and body.
  * @param {NextFunction} next - The `next` parameter is a function that is used to pass control to the
  * next middleware function in the request-response cycle. It is typically called at the end of the
  * `authorize` function to indicate that the current middleware has completed its processing and the
  * next middleware should be called.
- * @returns In the code snippet, the function `authorize` is being exported.
+ * @returns In the code snippet provided, the function is returning a response object with a status
+ * code and a JSON object containing a message. The specific response being returned depends on the
+ * condition being evaluated.
  */
 export function authorize(req: Request, res: Response, next: NextFunction) {
     try {
