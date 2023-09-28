3328c1df1643f89690f16e5162ad267bf87ff95c
bokuanT <e0701815@u.nus.edu>
2023-09-28 21:45:22 +0800

    Add simple login check without password authentication

--- frontend/contexts/AuthContext.tsx
+++ frontend/contexts/AuthContext.tsx
@@ -1,5 +1,6 @@
 import React, { createContext, ReactNode, useState, useContext, FormEvent } from 'react';
 import { useRouter } from 'next/router';
+import { fetchGet } from "@/utils/apiHelpers";
 
 interface AuthContextType {
     user: string | null;
@@ -14,10 +15,26 @@ export const AuthProvider = ({ children }: { children: ReactNode }) => {
     const router = useRouter();
 
     const login = async (email: string, password: string) => {
-        //TODO: implement login
-        setUser(email);
+        //TODO: proper login logic
+        // Fetch the list of users
+        const res = await fetchGet("/api/users");
         
+        // Check if the response is successful and contains data
+        if (res.status === 200 && res.data) {
+            // Check if the provided email exists in the list of users
+            const userExists = res.data.some((user: any) => user.email === email);
+            
+            if (userExists) {
+                setUser(email);
+            } else {
+                alert("No such user");
+            }
+        } else {
+            // Handle any errors or unexpected responses here
+            alert("Failed to fetch users or unexpected response format.");
+        }
     };
+    
 
     const logout = () => {
         setUser(null);
