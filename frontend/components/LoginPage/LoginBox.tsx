9c9cc6e6f4b9551703cc80ca29e7031207e75f36
Teo Hao Yu <thstoneyes@gmail.com>
2023-09-30 16:22:21 +0800

    Enable login using enter key

--- frontend/components/LoginPage/LoginBox.tsx
+++ frontend/components/LoginPage/LoginBox.tsx
@@ -9,7 +9,8 @@ const LoginBox = () => {
     const [email, setEmail] = useState("");
     const [password, setPassword] = useState("");
 
-    const handleLoginClick = async () => {
+    const handleSubmit = async (e: any) => {
+        e.preventDefault();
         await login(email, password);
         router.push("/");
     };
@@ -19,26 +20,29 @@ const LoginBox = () => {
             <div className="flex justify-center items-center w-full max-w-1000px absolute left-1/2 top-1/2 transform -translate-x-1/2 -translate-y-1/2">
                 <div className="flex-none w-2/5 text-center">
                     <div className="bg-white border-none rounded-lg shadow-md p-5 max-w-xs mx-auto">
-                        <input
-                            type="text"
-                            placeholder="Email address"
-                            className="w-full my-1 h-11 border border-dddfe2 text-1d2129 px-2 outline-none"
-                            value={email}
-                            onChange={(e) => setEmail(e.target.value)}
-                        />
-                        <input
-                            type="password"
-                            placeholder="Password"
-                            className="w-full my-1 h-11 border border-dddfe2 text-1d2129 px-2 outline-none"
-                            value={password}
-                            onChange={(e) => setPassword(e.target.value)}
-                        />
-                        <button
-                            className="w-full mt-1 h-11 bg-blue-600 rounded-md text-white text-xl font-bold"
-                            onClick={handleLoginClick}
-                        >
-                            Login
-                        </button>
+                        <form onSubmit={handleSubmit}>
+                            <input
+                                type="text"
+                                placeholder="Email address"
+                                className="w-full my-1 h-11 border border-dddfe2 text-1d2129 px-2 outline-none"
+                                value={email}
+                                onChange={(e) => setEmail(e.target.value)}
+                            />
+                            <input
+                                type="password"
+                                placeholder="Password"
+                                className="w-full my-1 h-11 border border-dddfe2 text-1d2129 px-2 outline-none"
+                                value={password}
+                                onChange={(e) => setPassword(e.target.value)}
+                            />
+                            <button
+                                className="w-full mt-1 h-11 bg-blue-600 rounded-md text-white text-xl font-bold"
+                                type="submit"
+                            >
+                                Login
+                            </button>
+                        </form>
+
                         <a
                             href="#"
                             className="block text-1877f2 text-sm no-underline pt-2 pb-5 border-b border-dadde1"
