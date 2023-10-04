9e0c15cf4c2091c54517dede44bd4bd43c3d1d6d
Tang Bo Kuan <e0701815@u.nus.edu>
2023-10-04 16:56:16 +0800

    Let server handle broadcasts to rooms and add test script

--- collaboration-service/src/index.ts
+++ collaboration-service/src/index.ts
@@ -48,4 +48,12 @@ io.use((socket: Socket, next) => {
 io.on('connection', (socket: Socket) => {
     const extendedSocket = socket as ExtendedSocket;
     console.log('New connection with roomId:', extendedSocket.roomId);
+    extendedSocket.join(extendedSocket.roomId);
+
+    // Listen for incoming event from this `extendedSocket` instance
+    extendedSocket.on('message', (message) => {
+        console.log('Message received from server:', message);
+        // Broadcast the message to all other clients in the room
+        extendedSocket.to(extendedSocket.roomId).emit('message', message);
+    });
 });
