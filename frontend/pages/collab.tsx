dd5ad5b5e7a2e9716296fb7334469db67b76d725
Teo Hao Yu <thstoneyes@gmail.com>
2023-10-09 17:30:03 +0800

    Refactor collab page code

--- frontend/pages/collab.tsx
+++ frontend/pages/collab.tsx
@@ -1,32 +1,8 @@
 import CollabPage from "@/components/CollabPage/CollabPage";
-import CodeEditor from "@/components/CollabPage/CodeEditor";
 import { NextPage } from "next";
-import { useMatching } from "@/contexts/MatchingContext";
-
-// types and enums
-import { LANGUAGE, COMMENT } from "@/utils/enums";
-import { useAuth } from "@/contexts/AuthContext";
 
 const Collab: NextPage = () => {
-    const user = useAuth();
-    const { roomId, language, difficulty, cancelMatching } = useMatching();
-    return (
-        // <>
-        //     <h1>Collab Room</h1>
-        //     <h1>room: {roomId}</h1>
-        //     <h1>language: {language}</h1>
-        //     <h1>difficulty: {difficulty}</h1>
-        // </>
-
-        <CodeEditor
-            language={language}
-            editorContent={
-                (language === "Python" ? "## " : "// ") +
-                "Type your solution here"
-            }
-            roomId={roomId}
-        />
-    );
+    return <CollabPage />;
 };
 
 export default Collab;
