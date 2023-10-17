e04bd0d5be040ebfd825d7ef66ec54d096a059a2
Tang Bo Kuan <e0701815@u.nus.edu>
2023-10-17 10:57:48 +0800

    Change question form

--- frontend/components/LoginPage/LoginBox.tsx
+++ frontend/components/LoginPage/LoginBox.tsx
@@ -1,15 +1,18 @@
 // Source: https://github.com/mui/material-ui/blob/v5.14.13/docs/data/material/getting-started/templates/sign-up/SignUp.tsx
-import Avatar from '@mui/material/Avatar';
-import Button from '@mui/material/Button';
-import CssBaseline from '@mui/material/CssBaseline';
-import TextField from '@mui/material/TextField';
-import Link from '@mui/material/Link';
-import Grid from '@mui/material/Grid';
-import Box from '@mui/material/Box';
+import {
+  Avatar,
+  Button,
+  CssBaseline,
+  TextField,
+  Link,
+  Grid,
+  Box,
+  Typography,
+  Container,
+  createTheme,
+  ThemeProvider,
+} from "@mui/material";
 import LockOutlinedIcon from '@mui/icons-material/LockOutlined';
-import Typography from '@mui/material/Typography';
-import Container from '@mui/material/Container';
-import { createTheme, ThemeProvider } from '@mui/material/styles';
 import React, { useState } from "react";
 import { useRouter } from "next/router";
 import { useAuth } from "../../contexts/AuthContext";
