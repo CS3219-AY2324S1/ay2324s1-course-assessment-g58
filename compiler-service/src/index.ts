9581be8d81db5baece459f8f49738c2810a56aa1
Tang Bo Kuan <e0701815@u.nus.edu>
2023-10-26 20:15:11 +0800

    Minor changes

--- compiler-service/src/index.ts
+++ compiler-service/src/index.ts
@@ -1,5 +1,4 @@
 import express, { Request, Response } from 'express';
-import router from './routes/index';
 import * as amqp from 'amqplib';
 import { compileCode } from './services/compiler.service';
 
