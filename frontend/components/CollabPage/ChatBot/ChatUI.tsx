ea89706aafcf58b12d7c8f427743e132ad4eb415
Tang Bo Kuan <e0701815@u.nus.edu>
2023-11-02 19:47:17 +0800

    Update css of chatbot

--- frontend/components/CollabPage/ChatBot/ChatUI.tsx
+++ frontend/components/CollabPage/ChatBot/ChatUI.tsx
@@ -16,6 +16,14 @@ const messages: RawMessageData[] = [
     { id: 1, text: "Hi there!", sender: "bot" },
     { id: 2, text: "Hello!", sender: "user" },
     { id: 3, text: "How can I assist you today?", sender: "bot" },
+    { id: 3, text: "How can I assist you today?", sender: "bot" },
+    { id: 3, text: "How can I assist you today?", sender: "bot" },
+    { id: 3, text: "How can I assist you today?", sender: "bot" },
+    { id: 3, text: "How can I assist you today?", sender: "bot" },
+    { id: 3, text: "How can I assist you today?", sender: "bot" },
+    { id: 3, text: "How can I assist you today?", sender: "bot" },
+    { id: 3, text: "How can I assist you today?", sender: "bot" },
+    { id: 3, text: "How can I assist you today?", sender: "bot" },
 ];
 
 function ChatUI() {
@@ -34,43 +42,46 @@ function ChatUI() {
 
     return (
         <Box
-        sx={{
-            height: "100vh",
-            display: "flex",
-            flexDirection: "column",
-            bgcolor: "grey.200",
-        }}
+            sx={{
+                height: "100%",
+                display: "flex",
+                flexDirection: "column",
+                bgcolor: "grey.200",
+            }}
         >
-        <Box sx={{ flexGrow: 1, overflow: "auto", p: 2 }}>
-            {messages.map((message) => (
-            <Message message={message} />
-            ))}
-        </Box>
-        <Box sx={{ p: 2, backgroundColor: "background.default" }}>
-            <Grid container spacing={2}>
-                <Grid item xs={10}>
-                    <TextField
-                        size="small"
-                        fullWidth
-                        placeholder="Type a message"
-                        variant="outlined"
-                        value={input}
-                        onChange={handleInputChange}
-                    />
+            <Box
+                sx={{ height: "300px", flexGrow: 1, overflow: "auto", p: 2 }}
+            >
+                {messages.map((message) => (
+                <Message message={message} />
+                ))}
+            </Box>
+            <Box sx={{ p: 2, backgroundColor: "background.default" }}>
+                <Grid container spacing={2}>
+                    <Grid item xs={9}>
+                        <TextField
+                            size="small"
+                            fullWidth
+                            placeholder="Ask me anything!"
+                            variant="outlined"
+                            value={input}
+                            onChange={handleInputChange}
+                            multiline
+                        />
+                    </Grid>
+                    <Grid item xs={3}>
+                        <Button
+                            fullWidth={false}
+                            size="small"
+                            color="primary"
+                            variant="contained"
+                            endIcon={<SendIcon />}
+                            onClick={handleSend}
+                        >
+                        </Button>
+                    </Grid>
                 </Grid>
-                <Grid item xs={2}>
-                    <Button
-                        fullWidth
-                        color="primary"
-                        variant="contained"
-                        endIcon={<SendIcon />}
-                        onClick={handleSend}
-                    >
-                    Send
-                    </Button>
-                </Grid>
-            </Grid>
-        </Box>
+            </Box>
         </Box>
     );
 };
