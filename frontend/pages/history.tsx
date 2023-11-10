8d1892492cfe6400b695288b14d7ef2522a65846
ryanchua00 <ryanchuahj@gmail.com>
2023-11-10 22:48:15 +0800

    Remove frontend components

--- frontend/pages/history.tsx
+++
@@ -1,8 +0,0 @@
-import HistoryPage from "@/components/HistoryPage/HistoryPage";
-import { NextPage } from "next";
-
-const History: NextPage = () => {
-    return <HistoryPage />;
-};
-
-export default History;
