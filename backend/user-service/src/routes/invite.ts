edc5bf47d7a5b16fbbfe70a172d00c727f620d0b
ryanchua00 <ryanchuahj@gmail.com>
2023-11-11 00:06:30 +0800

    Remove import

--- backend/user-service/src/routes/invite.ts
+++ backend/user-service/src/routes/invite.ts
@@ -1,6 +1,5 @@
 import express, { Request, Response } from "express";
 import { PrismaClient } from "@prisma/client";
-import axios from "axios";
 
 const prisma = new PrismaClient({
     log: ["query", "info", "warn", "error"],
